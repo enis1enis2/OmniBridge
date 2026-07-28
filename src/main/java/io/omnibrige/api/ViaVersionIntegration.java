687f1bf3386b7009b0a32c88fbc63335d7d50af1
Enis Polat The Github Coder <enispolat64@gmail.com>
2026-07-28 16:14:10 +0300

    chore: add GPL-3.0 license and headers
    - Add LICENSE file (GPL-3.0)
    - Add SPDX license headers to all 27 Java source files
    - Add <licenses>, <developers>, <scm> blocks to pom.xml
    - Add license field to plugin.yml and velocity-plugin.json
    - Fix website URL in plugin.yml to match GitHub repo
    - All 47 tests passing

--- src/main/java/io/omnibrige/api/ViaVersionIntegration.java
+++ src/main/java/io/omnibrige/api/ViaVersionIntegration.java
@@ -1,3 +1,10 @@
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * OmniBridge - Universal cross-version and cross-platform connectivity
+ * Copyright (c) 2026 OmniBridge Contributors
+ */
+
 package io.omnibrige.api;
 
 import org.bukkit.Bukkit;
